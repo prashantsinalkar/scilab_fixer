99b0fe340368e306679060c44979aca5154b52be
prashantsinalkar <prashantsinalkar@gmail.com>
2016-09-06 15:56:53 +0530

    formated the code

--- js/selection.js
+++ js/selection.js
@@ -1,26 +1,27 @@
 function getSelectionText(divID) {
-    var selectedText = "";
-    if (window.getSelection) {
-        var sel = window.getSelection();
-        var div = document.getElementById(divID);
-
-        if (sel.rangeCount) {
-            // Get the selected range
-            var range = sel.getRangeAt(0);
-            // Check that the selection is wholly contained within the div text
-            // if (range.commonAncestorContainer == div.firstChild) {
-                var selectedText = range.toString();
-            // }
-        }
-    }
-    return selectedText;
+	var selectedText = "";
+	if (window.getSelection) {
+		var sel = window.getSelection();
+		var div = document.getElementById(divID);
+		if (sel.rangeCount) {
+			// Get the selected range
+			var range = sel.getRangeAt(0);
+			// Check that the selection is wholly contained within the div text
+			// if (range.commonAncestorContainer == div.firstChild) {
+			var selectedText = range.toString();
+			// }
+		}
+	}
+	return selectedText;
 }
-$(document).ready(function() {
-    $(".fix-caption-code").mousedown(function() {
-        $("#edit-caption").val("");
-    });
-    $(".fix-caption-code").mouseup(function() {
-        quotedText = getSelectionText("#fix-caption-code");
-        $("#edit-caption").val(quotedText);
-    });
-});
+(function($) {
+	$(document).ready(function() {
+		$(".fix-caption-code").mousedown(function() {
+			$("#edit-caption").val("");
+		});
+		$(".fix-caption-code").mouseup(function() {
+			quotedText = getSelectionText("#fix-caption-code");
+			$("#edit-caption").val(quotedText);
+		});
+	});
+})(jQuery);
